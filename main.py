32ba1c581316ea2ece06b3c048e7e7ba46f8ab88
akuryanov <alexandrkuryanov@yandex.ru>
2024-10-18 15:12:03 +0400

    18/10

---
+++ main.py
@@ -0,0 +1,19 @@
+# 1st program
+print((9 ** 0.5) * 5)
+
+# 2st program
+print((9.99 > 9.98) and (1000 != 1000.1))
+
+# 3st program
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print((2 * 2 + 2) == (2 * (2 + 2)))
+
+# 4st program
+a = '123.456'
+a = float(a)
+a = a * 10
+a = a % 10
+a = int(a)
+print(a)
+
