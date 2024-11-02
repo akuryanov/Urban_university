9bf37daa1db81ed982a5a7e5f7c61d63de92fa24
akuryanov <alexandrkuryanov@yandex.ru>
2024-11-02 11:14:23 +0400

    18/10

---
+++ Module_2/Module_2_if.py
@@ -0,0 +1,26 @@
+# name = input('Введите Вашу имя: ')
+# if name == 'Urban':
+#     print('Здравствуйте Администратор:')
+# if name == 'Денис':
+#     print('Здравствуйте преподаватель')
+# else:
+#     print('Привет', name)
+
+# Задача
+# Вводится число
+# Если число делится на 3, выводится Fizz
+# Если число делится на 5, выводится Buzz
+# Если число делится и на 3 и на 5 выводится FizzBuzz
+
+# number = int(input('Введите число: '))
+#
+# if number % 3 and number % 5 == 0:
+#     print('FizzBuzz')
+# elif number % 3 == 0:
+#     print('Fizz')
+# elif number % 5 == 0:
+#     print('Buzz')
+# else:
+#     print('Число не делится ни на 3, ни на 5')
+
+# ДЗ
