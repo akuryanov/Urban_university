666ab10ea268c8b62e8dcff9b85e85a8d8c9366c
akuryanov <alexandrkuryanov@yandex.ru>
2024-11-07 11:29:31 +0400

    18/10

--- Module_2/Module_2_hard.py
+++ Module_2/Module_2_hard.py
@@ -1,16 +1,19 @@
 import random
 
 result = ''
+
+a = ''
 first_fild = random.randint(3, 20) # генерируем случайное целое число от 3 до 20 для первого поля
 print('Число в первом поле: ', first_fild)
-
 for i in range(1, first_fild):
     for j in range(i, first_fild):
         if i != j:
             if first_fild % (i + j) == 0:
-                pair_of_number = str(i) + str(j)
-                result += pair_of_number
-
+                a = str(i) + str(j)
+                print(a)
+                result += a
 
 print(result)
+while True:
+    pass
 
