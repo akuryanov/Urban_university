e2b2c617ab7bc7c1c23875436d81d4513b333a5a
akuryanov <alexandrkuryanov@yandex.ru>
2024-12-03 09:13:00 +0400

    18/10

--- module_5/module_5_1.py
+++ module_5/module_5_1.py
@@ -9,23 +9,11 @@ class House:
         else:
             for i in range(1, new_floor + 1):
                 print(i)
-    def __len__(self):
-        return self.number_of_floors
-
-    def __str__(self): # Для варианта вывода 3
-        return f'Название: {self.name}, количество этажей {self.number_of_floors}'
-
-# h1 = House('ЖК Горский', 18)
-# h2 = House('Домик в деревне', 2)
-#
-# h1.go_to(5)
-# h2.go_to(10)
-
-h1 = House('ЖК Эльбрус', 10)
-h2 = House('ЖК Акация', 20)
 
 
-print(h1)
-print(h2)
-print(len(h1))
-print(len(h2))
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+
+h1.go_to(5)
+h2.go_to(10)
+
