6516a3bebddcea2dea9ff2958d929401d2e814d5
akuryanov <alexandrkuryanov@yandex.ru>
2024-12-03 09:11:09 +0400

    18/10

--- module_5/module_5_1.py
+++ module_5/module_5_1.py
@@ -12,9 +12,6 @@ class House:
     def __len__(self):
         return self.number_of_floors
 
-    # def __str__(self): # Для вариантов вывода 1 и 2
-    #     return self.name
-
     def __str__(self): # Для варианта вывода 3
         return f'Название: {self.name}, количество этажей {self.number_of_floors}'
 
@@ -27,15 +24,7 @@ class House:
 h1 = House('ЖК Эльбрус', 10)
 h2 = House('ЖК Акация', 20)
 
-# Вариант вывода 1
-# print(f'{h1.name}, количество этажей {h1.number_of_floors}')
-# print(f'{h2.name}, количество этажей {h2.number_of_floors}')
 
-# Вариант вывода 2
-# print(f'{str(h1)}, количество этажей {len(h1)}')
-# print(f'{str(h1)}, количество этажей {len(h1)}')
-
-# Вариант вывода 3
 print(h1)
 print(h2)
 print(len(h1))
