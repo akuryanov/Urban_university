aa95bf7568983742c51e8dd3e1eba64f150f12ec
akuryanov <alexandrkuryanov@yandex.ru>
2024-12-04 10:59:27 +0400

    18/10

--- module_5/classanobject.py
+++ module_5/classanobject.py
@@ -10,8 +10,6 @@ class Human:
         self.age = age
         self.say_info()
 
-
-
     def say_info(self):
         print(f'Привет, меня зовут {self.name}, мне {self.age}')
 
@@ -22,13 +20,35 @@ class Human:
     def __len__(self):
         return self.age # Возвращаем возраст
 
-    def __del__(self): # Деструктор класса удаляет объекты из памяти
-        print(f'{self.name} ушёл')
+    def __str__(self): # ПЭтот метод определяет строковое представление нашего объекта
+        return f'Привет, меня зовут {self.name}'
+
+    # Перегрузка операторов
+    # Часто в операциях участвуют два объекта.
+    # Например, при сравнении одного человека с другим или одного числа с другим.
+    # В операциях, таких как вычитание, сложение или умножение, также принимают участие два элемента.
+    # В этом контексте можно представить, что self — это один участник операции, а other — второй
+
+    def __lt__(self, other): # Метод '__lt__()' можно интерпретировать как «меньше чем» (Lower than)
+        return self.age < other.age # Возвращает True если условие выполняется
+
+    def __gt__(self, other): # Метод '__gt__()' интерпретируется как «больше чем» (Greater than)
+        return self.age > other.age  # Возвращает True если условие выполняется
+
+    def __eq__(self, other): # можно реализовать перегрузку оператора равенства с помощью метода '__eq__()'.
+        # Этот метод проверяет, равен ли 'self.age' значению 'other.age'
+        return self.name == other.name and self.age == other.age  # Возвращает True если условие выполняется
+
+
+
+    def __del__(self):  # Деструктор класса удаляет объекты из памяти
+            print(f'{self.name} ушёл')
+
 
 den = Human('Денис', 22)
-max = Human('Максим', 23)
+max = Human('Максим', 22)
 
-del den
+# del den
 # Атрибуты, или характеристики, можно задавать и вручную
 # den.surname = 'Попов'
 # print(den.surname)
@@ -41,5 +61,8 @@ del den
 # den.say_info()
 # max.say_info()
 # birthday()
+print(den)
+print(den < max)
 max.birthday()
+print(den < max)
 
