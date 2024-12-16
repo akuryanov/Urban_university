231536987c44b396243580100b57cfdc39171d61
akuryanov <alexandrkuryanov@yandex.ru>
2024-12-16 10:52:25 +0400

    18/10

--- module_6/module_6.py
+++ module_6/module_6.py
@@ -1,5 +1,7 @@
 class Human:
     head = True
+    _legs = True # _ перед именем делает это имя только для локального использования
+    __arms = True # __ защищает имя от переопределения в дочерних классах
 
     # def __init__(self):
     #     self.about()
@@ -7,24 +9,30 @@ class Human:
     def sey_hello(self):
         print('Hello!')
 
+    def about (self):
+        print(self.head)
+        print(self._legs)
+        print(self.__arms)
+
 class Student(Human): # В круглых скобках указывается от какого класса наследуется класс
                       # Human - базовый класс, Student - наследуется от Human
-
-    def about(self):
-        print('I am student')
+    pass
+    # def about(self):
+    #     print('I am student')
 
 class Teacher(Human):
     pass
 
 
-# human = Human()
+human = Human()
 student = Student()
-teacher = Teacher()
-student.sey_hello()
-teacher.sey_hello()
+# teacher = Teacher()
+# student.sey_hello()
+# teacher.sey_hello()
 
 # print(human.head)
 student.about()
 
-print(student.head)
-# human.about()
+# print(student.head)
+human.about()
+print(dir(human))
