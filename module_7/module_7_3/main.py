44d698e9825bc47dc9d583362d9a3145f9c4ae70
akuryanov <alexandrkuryanov@yandex.ru>
2024-12-23 16:31:57 +0400

    18/10

--- module_7/module_7_3/main.py
+++ module_7/module_7_3/main.py
@@ -1,10 +1,3 @@
-import string
-import re
-from string import punctuation
-
-from numpy.lib.recfunctions import join_by
-
-
 class WordsFinder:
     def __init__(self, *file):
         self.file = file
@@ -19,62 +12,62 @@ class WordsFinder:
                 for line in file:
                     line = line.lower() # Переводим строки в нижний регистр
                     str_ += line # Объединяем все строки в одну
-
                 for i in str_:
                     if i in punct:
-                        str_ = str_.replace(i, '') # Удаляем знаки препинания
-
+                        str_ = str_.replace(i, '')  # Удаляем знаки препинания
                 dict_ = str_.split() # Переводим строку в список слов
+
                 all_words.update({file_name: dict_}) # создаем словарь, где ключ - имя файла, значение - список слов файла
+                str_ = ''
 
         return all_words
 
     def find(self, word):
         word = word.lower()
-        for file in self.file:
-            content = self.get_all_words()
+        dict_ = {}
+        content = self.get_all_words()
+        for file, words in content.items():
+            if word in words:
+                positions = words.index(word)
+                dict_.update({file: positions + 1})
+        return dict_
 
-        # print(content.items())
+    def count(self, word):
+        dict_ = {}
+        word = word.lower()
+        content = self.get_all_words()
+        for self.file, words in content.items():
+            # counts = 0
+            if word in words:
+                counts = words.count(word)
+                dict_.update({self.file: counts})
 
-        # for file_name, words in content.items():
-        #     print(file_name)
-        #     print(words)
-            # if word in words:
-            #     positions = words.index(word)
-            #     return {file_name: positions + 1}
-    #
-    # def count(self, word):
-    #     word = word.lower()
-    #     content = self.get_all_words()
-    #     for name, words in content.items():
-    #         if word in words:
-    #             counts = words.count(word)
-    #             return {name: counts}
+        return dict_
 
 
 finder2 = WordsFinder('test_file.txt')
-# print(finder2.get_all_words())
+print(finder2.get_all_words())
 
 # finder2.get_all_words()
 #
-# print(finder2.find('TEXT'))
-# print( finder2.count('Text'))
+print(finder2.find('TEXT'))
+print( finder2.count('Text'))
 
-# finder2 = WordsFinder('file_1.txt', 'file_2.txt', 'file_3.txt')
-#
-# print(finder2.get_all_words())
-# print(finder2.find('Text_2_2'))
-# print(finder2.find('Text_1_1'))
-# print(finder2.find('Text_3_2'))
-# print(finder2.find('Text_3_7'))
-# print(finder2.count('Text_3_7'))
+finder2 = WordsFinder('file_1.txt', 'file_2.txt', 'file_3.txt')
+
+print(finder2.get_all_words())
+print(finder2.find('Text_2_2'))
+print(finder2.find('Text_1_1'))
+print(finder2.find('Text_3_2'))
+print(finder2.find('Text_3_7'))
+print(finder2.count('Text_3_7'))
 
 finder1 = WordsFinder('Walt Whitman - O Captain! My Captain!.txt',
                       'Rudyard Kipling - If.txt',
                       'Mother Goose - Monday’s Child.txt')
 print(finder1.get_all_words())
-# print(finder1.find('The'))
-# print(finder1.count('THE'))
+print(finder1.find('The'))
+print(finder1.count('THE'))
 
 
 
