0d012603c5a9b4be0bc8bb8edd79eae8e91e82b3
akuryanov <alexandrkuryanov@yandex.ru>
2024-12-23 08:43:48 +0400

    18/10

---
+++ module_7/module_7_2/main.py
@@ -0,0 +1,25 @@
+def custom_write(file_name, strings):
+    file = open(file_name, 'w')
+    strings_positions = {}
+
+    for i in range(len(strings)):
+        tuple_ = (i+1, file.tell())
+        file.write(str(strings[i]) + '\n')
+        str_ = {tuple_: strings[i]}
+        strings_positions.update(str_)
+
+    file.close()
+    return strings_positions
+
+
+info = [
+    'Text for tell.',
+    'Используйте кодировку utf-8.',
+    'Because there are 2 languages!',
+    'Спасибо!'
+    ]
+
+result = custom_write('test.txt', info)
+
+for elem in result.items():
+    print(elem)
