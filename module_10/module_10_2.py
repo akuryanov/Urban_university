07c43313d84f24a887363a6a946854822850fa9e
akuryanov <alexandrkuryanov@yandex.ru>
2025-01-13 15:06:57 +0400

    18/10

--- module_10/module_10_2.py
+++ module_10/module_10_2.py
@@ -26,9 +26,10 @@ class Knight(threading.Thread):
 
 first_knight = Knight('Sir Lancelot', 10)
 second_knight = Knight("Sir Galahad", 20)
-first_knight.start()
 
+first_knight.start()
 second_knight.start()
+
 first_knight.join()
 second_knight.join()
 print('Все битвы закончились!')
