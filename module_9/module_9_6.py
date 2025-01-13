0589e2c11ce91fc3c706853101bc6639f8322051
akuryanov <alexandrkuryanov@yandex.ru>
2025-01-13 09:28:06 +0400

    18/10

--- module_9/module_9_6.py
+++ module_9/module_9_6.py
@@ -1,9 +1,11 @@
+# Домашнее задание по теме "Генераторы"
+
 def all_variants(text):
     for i in range(len(text)):
         for j in range(len(text) - i):
             yield text[j:i + j + 1]
 
-a = all_variants("abc")
+a = all_variants("abcdef")
 
 for i in a:
     print(i)
